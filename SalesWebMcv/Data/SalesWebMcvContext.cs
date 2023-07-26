3851a84fa02566c2d4b6a625da7db35953dff7be
Gustavo Monnerat <gugammcv@gmail.com>
2023-07-26 20:22:19 -0300

    MySQL adaptation and first migration

---
+++ SalesWebMcv/Data/SalesWebMcvContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMcv.Models;
+
+namespace SalesWebMcv.Data
+{
+    public class SalesWebMcvContext : DbContext
+    {
+        public SalesWebMcvContext (DbContextOptions<SalesWebMcvContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMcv.Models.Department> Department { get; set; } = default!;
+    }
+}
--- SalesWebMcv/Program.cs
+++ SalesWebMcv/Program.cs
@@ -1,4 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SalesWebMcv.Data;
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("SalesWebMvcContext"); // Getting connection string from appsettings.json "ConnectionStrings": {"SalesWebMvcContext"}
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 34)); // Server version: 8.0.34 MySQL Community Server
+builder.Services.AddDbContext<SalesWebMcvContext>(options => options.UseMySql(connectionString, serverVersion));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
