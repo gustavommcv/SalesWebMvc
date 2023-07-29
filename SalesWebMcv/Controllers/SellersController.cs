dfa129d87d20b486ace4e6323dfc2e990dedf226
Gustavo Monnerat <gugammcv@gmail.com>
2023-07-29 17:22:39 -0300

    Validation

--- SalesWebMcv/Controllers/SellersController.cs
+++ SalesWebMcv/Controllers/SellersController.cs
@@ -29,6 +29,11 @@ namespace SalesWebMvc.Controllers {
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller) {
+            if (!ModelState.IsValid) {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Departments = departments, Seller = seller };
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -74,6 +79,12 @@ namespace SalesWebMvc.Controllers {
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller) {
+            if (!ModelState.IsValid) { 
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Departments = departments, Seller = seller };
+                return View(viewModel); 
+            }
+
             if (id != seller.Id) { return RedirectToAction(nameof(Error), new { message = "Id mismatch" }); }
 
             try {
