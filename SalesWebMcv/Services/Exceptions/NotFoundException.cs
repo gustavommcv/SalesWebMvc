0c62d1a5e94ca9d3e866dda3861038af7157707a
Gustavo Monnerat <gugammcv@gmail.com>
2023-07-29 16:20:09 -0300

    Update seller and custom service exception

---
+++ SalesWebMcv/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,5 @@
+﻿namespace SalesWebMvc.Services.Exceptions {
+    public class NotFoundException : ApplicationException {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
