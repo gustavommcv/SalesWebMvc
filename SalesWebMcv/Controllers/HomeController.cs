7bfce7bdcb101d10bbcfce96cec2aec4e4c13c58
Gustavo Monnerat <gugammcv@gmail.com>
2023-07-30 01:14:34 -0300

    A few changes to some views

--- SalesWebMcv/Controllers/HomeController.cs
+++ SalesWebMcv/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace SalesWebMvc.Controllers {
             return View();
         }
 
+        public IActionResult Contact() {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
